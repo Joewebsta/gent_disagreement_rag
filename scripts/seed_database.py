dc28376477d07264b26a421d3d2f6ddb8a671d90
Joe Webster <joseph.scott.webster@gmail.com>
2025-10-06 19:43:21 -0400

    Add new episode entry for AGD-182 to seed_database.py

--- scripts/seed_database.py
+++ scripts/seed_database.py
@@ -85,10 +85,12 @@ def seed_episodes(db_manager: DatabaseManager) -> None:
                 date_published
             ) VALUES 
                 ('180', 'A SCOTUS ''24-''25" term review with Professor Jack Beermann', 'AGD-180.mp3', '2025-08-12'),
-                ('181', 'Six in Sixty: creeping authoritarianism', 'AGD-181.mp3', '2025-08-26')
+                ('181', 'Six in Sixty: creeping authoritarianism', 'AGD-181.mp3', '2025-08-26'),
+                ('182', 'How tariffs are affecting the global economy and geopolitics with Lydia DePillis', 'AGD-182.mp3', '2025-02-09'),
             ON CONFLICT (episode_number) DO NOTHING;
             """
         )
+
         conn.commit()
         logger.info("Episodes data seeded successfully!")
     except Exception as e:
